c68f18826d16f356db8a5501e542f3b1a7405a48
Miles Crighton <miles.crighton@gmail.com>
2023-03-19 11:31:45 +0000

    Update hortis dir cmds

--- functions/tmux-hortis.fish
+++ functions/tmux-hortis.fish
@@ -13,10 +13,10 @@ function tmux-hortis -d 'Open tmux config for Hortis development'
     tmux new-window -t $SESSION_NAME:3 -n 'servers'
     tmux split-window -h -t $SESSION_NAME:3
 
-    tmux send-keys -t $SESSION_NAME:1 C-z 'cd ~/work/botsoft-frontend && nvim .' Enter
-    tmux send-keys -t $SESSION_NAME:2 C-z 'cd ~/work/botsoft-api && nvim .' Enter
-    tmux send-keys -t $SESSION_NAME:3.0 C-z 'cd ~/work/botsoft-frontend' Enter
-    tmux send-keys -t $SESSION_NAME:3.1 C-z 'cd ~/work/botsoft-api' Enter
+    tmux send-keys -t $SESSION_NAME:1 C-z 'cd ~/work/hortis-frontend && nvim .' Enter
+    tmux send-keys -t $SESSION_NAME:2 C-z 'cd ~/work/hortis-api && nvim .' Enter
+    tmux send-keys -t $SESSION_NAME:3.0 C-z 'cd ~/work/hortis-frontend' Enter
+    tmux send-keys -t $SESSION_NAME:3.1 C-z 'cd ~/work/hortis-api' Enter
 
     # Attach to session
     tmux attach-session -t $SESSION_NAME
